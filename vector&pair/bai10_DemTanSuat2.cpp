a8eb31d86ea37cb9fcb09ee1aff4e9096674822f
HoanggLB2k2 <lebaohoangth36@gmail.com>
2024-02-26 09:07:18 +0700

    Completed

--- vector&pair/bai10_DemTanSuat2.cpp
+++ vector&pair/bai10_DemTanSuat2.cpp
@@ -5,6 +5,31 @@ typedef long long ll;
 int main(int argc, char const *argv[])
 {
 /*code*/
- 
+vector<pair<char,int>> vec;
+int n;
+cin>>n;
+for(int i = 0; i < n ;i++)
+{
+    char x; cin>>x;
+    bool check = true;
+    for(int j = 0; j < vec.size(); j++)
+    {
+        if (vec[j].first == x)
+        {
+            vec[j].second++;
+            check = false;
+            break;
+        }
+    }
+    if (check == true)
+    {
+        vec.push_back(make_pair(x,1));
+    }
+}
+cout<<vec.size()<<endl;
+for(pair<char,int> it : vec)
+{
+    cout<<it.first<<" "<<it.second<<endl;
+}
 return 0;
 }
