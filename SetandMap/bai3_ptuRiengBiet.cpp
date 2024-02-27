cd2cf9918eab925f26fb73a762ae0415d48d25f6
HoanggLB2k2 <lebaohoangth36@gmail.com>
2024-02-28 00:51:11 +0700

    Neu ptu chua co trong set thi in ra

--- SetandMap/bai3_ptuRiengBiet.cpp
+++ SetandMap/bai3_ptuRiengBiet.cpp
@@ -1,5 +1,6 @@
 /*
 Cho dãy số A[] gồm có N phần tử, bạn hãy liệt kê các giá trị xuất hiện trong dãy theo thứ tự xuất hiện, mỗi giá trị chỉ liệt kê một lần.
+Gợi ý : Dùng set để lưu các phần tử đã được in ra trước đó, gặp phần tử a[i] thì kiểm tra nhanh trong set có giá trị a[i] chưa để in ra
 */
 #include<bits/stdc++.h>
 using namespace std;
