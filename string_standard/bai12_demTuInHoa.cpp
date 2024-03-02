bd276a8bb56ae4666b4f5ae92771e07026e6c101
HoanggLB2k2 <lebaohoangth36@gmail.com>
2024-03-02 14:44:54 +0700

    Completed

--- string_standard/bai12_demTuInHoa.cpp
+++ string_standard/bai12_demTuInHoa.cpp
@@ -1,13 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+bool checkUpper(string s)
+{
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (islower(s[i]))
+        {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
 // main
 int main(int argc, char const *argv[])
 {
-/*code*/
-string s;
-getline(cin,s);
-char *token = strtok(s," ");
-
-return 0;
+    /*code*/
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string word;
+    int count = 0;
+    while (ss >> word)
+    {
+        /* code */
+        if (checkUpper(word))
+            count++;
+    }
+    cout << count;
+    return 0;
 }
